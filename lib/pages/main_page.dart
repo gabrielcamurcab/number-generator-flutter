b84979ff7c32c067bd7776c32b0c8104c1876bd9
Gabriel Camurça <gabriel@brtech.dev>
2023-12-08 15:37:05 -0300

    Implements BottomNavigationBar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:trilhaapp/pages/cadastral_data.dart';
+import 'package:trilhaapp/pages/page1.dart';
+import 'package:trilhaapp/pages/page2.dart';
+import 'package:trilhaapp/pages/page3.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+  const MainPage({Key? key});
 
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  int pagePosition = 0;
+  PageController controller = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Main page")),
-      drawer:  Drawer(
+      drawer: Drawer(
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(10),
@@ -21,39 +27,77 @@ class _MainPageState extends State<MainPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text("Menu principal",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 const Divider(),
                 InkWell(
                   child: Container(
                     width: double.infinity,
                     padding: const EdgeInsets.all(10),
-                    child: const Text("Dados cadastrais")),
+                    child: const Text("Dados cadastrais"),
+                  ),
                   onTap: () {
                     Navigator.pop(context);
                     Navigator.push(
-                      context, MaterialPageRoute(
+                      context,
+                      MaterialPageRoute(
                         builder: (context) => const CadastralDataPage(
                           text: "Dados Cadastrais",
-                          data: ["Nome", "Endereço"])));
-                  }),
+                          data: ["Nome", "Endereço"],
+                        ),
+                      ),
+                    );
+                  },
+                ),
                 const Expanded(child: Column()),
                 InkWell(
                   child: Container(
                     width: double.infinity,
                     padding: const EdgeInsets.all(10),
-                    child: const Text("Configurações")),
-                  onTap: () {}),
+                    child: const Text("Configurações"),
+                  ),
+                  onTap: () {},
+                ),
                 const SizedBox(height: 10),
                 InkWell(
                   child: Container(
                     width: double.infinity,
                     padding: const EdgeInsets.all(10),
-                    child: const Text("Termos de uso e privacidade")),
-                  onTap: () {}),
+                    child: const Text("Termos de uso e privacidade"),
+                  ),
+                  onTap: () {},
+                ),
               ],
             ),
           ),
         ),
+      ),
+      body: PageView(
+        controller: controller,
+        onPageChanged: (value) {
+          setState(() {
+            pagePosition = value;
+          });
+        },
+        children: const [
+          Page1(),
+          Page2(),
+          Page3(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            controller.jumpToPage(value);
+          });
+        },
+        currentIndex: pagePosition,
+        items: const [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: "Atualizações", icon: Icon(Icons.update)),
+          BottomNavigationBarItem(label: "Ajuda", icon: Icon(Icons.help)),
+        ],
       ),
     );
   }
