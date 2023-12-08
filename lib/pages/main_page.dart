cddf534d8d91f973374a29664db4c05cec060da3
Gabriel Camurça <gabriel@brtech.dev>
2023-12-08 14:29:46 -0300

    Create main page

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Main page")),
+      drawer:  Drawer(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Menu principal",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const Divider(),
+                InkWell(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text("Dados cadastrais")),
+                  onTap: () {}),
+                const Expanded(child: Column()),
+                InkWell(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text("Configurações")),
+                  onTap: () {}),
+                const SizedBox(height: 10),
+                InkWell(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text("Termos de uso e privacidade")),
+                  onTap: () {}),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
